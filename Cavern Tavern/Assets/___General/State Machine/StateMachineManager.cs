63d4b41bc52e2e6a04af8c57114ce2e2fd0b2070
uppedthumbs <61210489+uppedthumbs@users.noreply.github.com>
2023-01-18 00:59:05 -0800

    Created little demon part 1
    Little demon part 1

--- Cavern Tavern/Assets/___General/State Machine/StateMachineManager.cs
+++ Cavern Tavern/Assets/___General/State Machine/StateMachineManager.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//Helps activate/cycle the behaviors of a game object
+
 public class StateMachineManager : MonoBehaviour
 {
     public State CurrentState;
