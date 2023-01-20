46101f98f327f31024224a609843bfe2be0ba7b6
savagebananas <bruno7405@gmail.com>
2023-01-20 15:45:34 -0800

    Renamed Projectile script methods

--- Cavern Tavern/Assets/__Combat System/Enemy Scripts/Behaviors/RangedAttack.cs
+++ Cavern Tavern/Assets/__Combat System/Enemy Scripts/Behaviors/RangedAttack.cs
@@ -48,7 +48,7 @@ public class RangedAttack : State
         }
     }
 
-    void ShootProjectile()
+    void ShootSingleProjectile()
     {
         //Angles
         Vector2 enemyToPlayerVector = playerBase.transform.position - transform.position;
@@ -70,12 +70,12 @@ public class RangedAttack : State
         //This adjust the buller count to actually fire the right amount of bullets
         numberOfBullets--;
 
-        //Get vector towards player
-         Vector2 enemyToPlayerVector = playerBase.transform.position - transform.position;
+        //Vector and angle towards player
+        Vector2 enemyToPlayerVector = playerBase.transform.position - transform.position;
         enemyToPlayerVector.Normalize();
         float enemyToPlayerAngle = Mathf.Atan2(enemyToPlayerVector.y, enemyToPlayerVector.x) * Mathf.Rad2Deg;
 
-        for(float i = -angleOfSpread/2; i <= angleOfSpread/2;i+= angleOfSpread/numberOfBullets){
+        for(float i = -angleOfSpread/2; i <= angleOfSpread/2; i+= angleOfSpread/numberOfBullets){
                 //Create projectile with a vector towards the player and all the other stats
                  GameObject projectile = Instantiate(projectilePrefab, transform.position, Quaternion.Euler(0, 0, enemyToPlayerAngle - 45));
                  Projectile projectileScript = projectile.GetComponent<Projectile>();
@@ -85,7 +85,7 @@ public class RangedAttack : State
 
                  //Adjust the projectiles vector so it fits the spread 
                  Vector2 direction = enemyToPlayerVector;
-                 projectileScript.direction = (Vector2)(Quaternion.Euler(direction.x,direction.y,direction.x+i) * direction);
+                 projectileScript.direction = (Vector2)(Quaternion.Euler(direction.x, direction.y, direction.x + i) * direction);
         }
 
     }
